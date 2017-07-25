96d1a5098b63e442c8081de82213074c5062c66f
eric-bennett <ericroybennett@gmail.com>
2017-07-24 18:36:37 -0700

    added name to header

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package edu.pcc.cis.helloworld;
 import java.util.Date;
 
 /**
- * Created by Eric on 7/24/2017.
+ * Created by Eric Bennett on 7/24/2017.
  */
 public class HelloWorld {
     public static void main(String[] args) {
